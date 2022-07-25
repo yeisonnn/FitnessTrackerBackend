fc42d12c16d3606cecb0b36645540a030cba7f19
Christian Factoriza <cfactoriza@gmail.com>
2022-07-25 10:33:10 -0500

    getUser function set

--- db/users.js
+++ db/users.js
@@ -4,7 +4,7 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  
+  try {
     const {
       rows: [user]
     } = await client.query(
@@ -16,11 +16,30 @@ async function createUser({ username, password }) {
       [username, password]
     );
     return user;
+  }
+  catch (error){
+    throw error;
+  }
 }
 // Return to add try/catch block
 
 async function getUser({ username, password }) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+        SELECT *
+        FROM users
+        WHERE username=$1 AND password=$2;
+      `,
+      [username, password]
+    );
 
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getUserById(userId) {
