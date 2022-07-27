95993d2becf14456d23576f08282c6c36dc4ff85
Christian Factoriza <cfactoriza@gmail.com>
2022-07-27 10:29:55 -0500

    API cors tests

--- api/index.js
+++ api/index.js
@@ -1,24 +1,28 @@
 const express = require('express');
+const cors = require('cors')
 const router = express.Router();
+const app = express()
+
+app.use(cors())
 
 // GET /api/health
-router.get('/health', async (req, res, next) => {
+router.get('/health', cors(), async (req, res, next) => {
+res.send(
+    "All is well"
+    
+)
 });
 
-// ROUTER: /api/users
+
+
 const usersRouter = require('./users');
 router.use('/users', usersRouter);
-
-// ROUTER: /api/activities
 const activitiesRouter = require('./activities');
 router.use('/activities', activitiesRouter);
-
-// ROUTER: /api/routines
 const routinesRouter = require('./routines');
 router.use('/routines', routinesRouter);
-
-// ROUTER: /api/routine_activities
 const routineActivitiesRouter = require('./routineActivities');
+
 router.use('/routine_activities', routineActivitiesRouter);
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 require("dotenv").config()
 const express = require("express")
+const morgan = require('morgan');
 const app = express()
 
+app.use(morgan('dev'));
+app.use(express.json());
+
 // Setup your Middleware and API Router here
+const apiRouter = require('./api');
+app.use('/api', apiRouter);
 
 module.exports = app;
