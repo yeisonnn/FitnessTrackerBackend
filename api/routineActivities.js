935d73eb5bbf13ae11cbedb3ea531134f582a6e8
Christian Factoriza <cfactoriza@gmail.com>
2022-07-28 15:55:07 -0500

    Fixed routine_activity Delete

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { updateRoutineActivity, getRoutineActivityById, getRoutineById } = require('../db');
+const { updateRoutineActivity, getRoutineActivityById, getRoutineById, destroyRoutineActivity } = require('../db');
 const { requireUser } = require('./utils');
 const router = express.Router();
 
 // PATCH /api/routine_activities/:routineActivityId
-router.patch(':routineActivityId', requireUser, async (req, res, next) =>{
+router.patch('/:routineActivityId', requireUser, async (req, res, next) =>{
 const {count, duration} = req.body
 const routineActivity = await getRoutineActivityById(req.params.routineActivityId)
 const routineId = routineActivity.routineId
@@ -34,5 +34,28 @@ const username = req.user.username
 })
 
 // DELETE /api/routine_activities/:routineActivityId
+router.delete('/:routineActivityId', requireUser, async (req, res, next)=>{
+    const routineActivity = await getRoutineActivityById(req.params.routineActivityId)
+    const routineId = routineActivity.routineId
+    const routine = await getRoutineById(routineId);
+    const username = req.user.username
+    const id = routineActivity.id
+
+    try {
+        if (routine.creatorId != req.user.id){
+            res.status(403)
+                next({
+                name: "You are not the Owner",
+                message: `User ${username} is not allowed to delete ${routine.name}`,
+                error: "There was an error",
+              })
+          }
+          const removedRoutineActivity = await destroyRoutineActivity(id)
+          res.send(routineActivity)
+
+    } catch ({name, message}){
+        next ({name, message})
+    }
+})
 
 module.exports = router;
