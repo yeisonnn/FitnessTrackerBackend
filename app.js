367f9973f03270e58c612ab3de21b4e3732b81c6
yeisson giraldo <yeisonnn@gmail.com>
2022-07-29 09:15:22 -0500

    test 404 passing

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const morgan = require("morgan");
-const client = require("./db/client");
+const morgan = require('morgan');
+const client = require('./db/client');
 const cors = require('cors');
 
 client.connect();
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
 
 const apiRouter = require('./api');
@@ -15,14 +15,17 @@ app.use('/api', apiRouter);
 
 // Setup your Middleware and API Router here
 app.use((error, req, res, next) => {
-    res.send({
-      name: error.name,
-      message: error.message,
-      error: error.message
-    });
+  res.send({
+    name: error.name,
+    message: error.message,
+    error: error.message,
   });
+});
 
-
-
+app.use(function (req, res, next) {
+  res.status(404).send({
+    message: 'Sorry cant find that!',
+  });
+});
 
 module.exports = app;
